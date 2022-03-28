a30856848d88e81c343360f4af2dd7a8a69711c4
Arpan Neupane <arpanneupane19@gmail.com>
2022-03-27 19:38:41 -0500

    pages are being displayed as they should be

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import CreateRoom from "./pages/CreateRoom";
 import JoinRoom from "./pages/JoinRoom";
+import Room from "./pages/Room";
 import Icon from "react-native-vector-icons/AntDesign";
+import { io } from "socket.io-client";
 
+const mode = "DEVELOPMENT";
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const socket = io("http://YOURAPIURLHERE/");
 
 export default function App() {
   return (
@@ -14,43 +20,82 @@ export default function App() {
       <NavigationContainer>
         <Tab.Navigator
           initialRouteName="CreateRoom"
-          screenOptions={{
-            headerShown: false,
+          screenOptions={({ route }) => ({
             tabBarStyle: {
               backgroundColor: "rgb(20,20,20)",
-              height: 100,
+              height: 90,
               borderTopColor: "transparent",
             },
-          }}
+            tabBarButton: ["Room"].includes(route.name)
+              ? () => null
+              : undefined,
+          })}
         >
           <Tab.Screen
             name="CreateRoom"
-            component={CreateRoom}
             options={{
+              title: "Create Room",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerStyle: {
+                backgroundColor: "rgb(10,10,10)",
+                shadowColor: "rgb(10,10,10)",
+              },
               tabBarLabel: "Create Room",
               tabBarLabelStyle: {
                 fontSize: 15,
                 color: "rgb(0,140,255)",
               },
               tabBarIcon: () => {
-                return <Icon name="edit" size={30} color="rgb(0,140,255)" />;
+                return <Icon name="edit" size={25} color="rgb(0,140,255)" />;
               },
             }}
-          />
+          >
+            {(props) => <CreateRoom {...props} mode={mode} socket={socket} />}
+          </Tab.Screen>
           <Tab.Screen
             name="JoinRoom"
-            component={JoinRoom}
             options={{
+              title: "Join Room",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerStyle: {
+                backgroundColor: "rgb(10,10,10)",
+                shadowColor: "rgb(10,10,10)",
+              },
               tabBarLabel: "Join Room",
               tabBarLabelStyle: {
                 fontSize: 15,
                 color: "rgb(0,140,255)",
               },
               tabBarIcon: () => {
-                return <Icon name="enter" size={30} color="rgb(0,140,255)" />;
+                return <Icon name="enter" size={25} color="rgb(0,140,255)" />;
               },
             }}
-          />
+          >
+            {(props) => <JoinRoom {...props} mode={mode} socket={socket} />}
+          </Tab.Screen>
+          <Tab.Screen
+            name="Room"
+            initialParams={{ code: null, name: null }}
+            options={{
+              title: "Room",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerStyle: {
+                backgroundColor: "rgb(10,10,10)",
+                shadowColor: "rgb(10,10,10)",
+              },
+              tabBarStyle: {
+                display: "none",
+              },
+            }}
+          >
+            {(props) => <Room {...props} mode={mode} socket={socket} />}
+          </Tab.Screen>
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
